98a9567c704c4eebf6e31a1586202276f1cc132a
AnonymousDonut <yuvalmasterglitch@gmail.com>
2024-11-23 22:56:15 +0200

    Add files via upload

---
+++ DontPlayGtag/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace DontPlayGtag
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.donut.gorillatag.dontplaygtag";
+        public const string Name = "DontPlayGtag";
+        public const string Version = "1.0.0";
+    }
+}
